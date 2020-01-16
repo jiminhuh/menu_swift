debe036f31463604d7f8c27a252fa122923ae98e
Jimin Huh <jiminhuh95@gmail.com>
2020-01-15 21:20:49 -0600

    ui build

---
+++ menu-swift/menu-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  menu-swift
+//
+//  Created by Jimin Huh on 1/15/20.
+//  Copyright © 2020 Jimin Huh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
